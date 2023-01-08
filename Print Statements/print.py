00d0ad0459404e5c8fcb04ffb4e15c4f9ed8d1b8
Adi Venkat <68367441+adivenkat05@users.noreply.github.com>
2023-01-09 05:07:24 +0530

    Initial commit

---
+++ Print Statements/print.py
@@ -0,0 +1,7 @@
+print("Hello World", end = '\t') # Default escape sequence is \n.
+
+print("Hello","World", sep = '_') # Separator.
+
+print("Hello "+"World") # Concatenation
+
+print("[*] Hello World \n" * 3) # Printing "[*] Hello World" 3 times.
