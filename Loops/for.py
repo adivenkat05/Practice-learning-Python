5aed43c493b8f5a1d9159a7bebf17208976ca519
Adi Venkat <68367441+adivenkat05@users.noreply.github.com>
2023-01-09 19:36:37 +0530

    Initial commit

---
+++ Loops/for.py
@@ -0,0 +1,10 @@
+# Syntax of range(initial, final)
+
+for i in range(1, 11):
+    print(f"[{i}] Hello World")
+
+print("\n")
+
+#        range(initial, final, step)
+for i in range(0, 11, 2):
+    print(f"[{i}] Hello World")
