da4bcac8bb566a682f5271d4ccec92a36f7a202d
Adi Venkat <68367441+adivenkat05@users.noreply.github.com>
2023-01-09 05:51:34 +0530

    Initial commit

---
+++ Operators/relational-op.py
@@ -0,0 +1,14 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print(a > b) # Greater than
+
+print(a < b) # Lesser than
+
+print(a <= b) # Lesser than or equals to
+
+print(a >= b) # Greater than or equals to
+
+print(a == b) # Is equals to 
+
+print(a != b) # Not equals to
