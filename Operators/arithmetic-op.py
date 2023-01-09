01203055c39be665f383b396c7ec7d40902d3a05
Adi Venkat <68367441+adivenkat05@users.noreply.github.com>
2023-01-09 05:49:38 +0530

    Initial commit

---
+++ Operators/arithmetic-op.py
@@ -0,0 +1,14 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print(f"Addition : {a + b}") # Addition
+
+print(f"Subtraction: {a - b}") # Subtraction
+
+print(f"Multiplication: {a * b}") # Multiplication
+
+print(f"Division: {a / b}") # Division
+
+print(f"Remainder: {a % b}") # Remainder
+
+print(f"Floor division: {a // b}") # Floor Division
