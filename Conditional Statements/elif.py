8dd51f4fe38c2535deb4d9f3a5455b45bc8dea21
Adi Venkat <68367441+adivenkat05@users.noreply.github.com>
2023-01-09 19:08:32 +0530

    Initial commit

---
+++ Conditional Statements/elif.py
@@ -0,0 +1,10 @@
+a = 200
+b = 33
+if b > a:    # Checks if a is greater than b, if it's true the if block executes
+    print("b is greater than a")
+
+elif a == b:
+    print("a and b are equal")
+
+else:
+    print("a is greater than b")
