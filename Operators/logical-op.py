6c2507043665d0e32ae7a77648f090165db292c1
Adi Venkat <68367441+adivenkat05@users.noreply.github.com>
2023-01-09 06:00:57 +0530

    Initial commit

---
+++ Operators/logical-op.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print(f"(a>b) and (a>b) = {(a>b) and (a>b)}") # AND
+
+print(f"(a>b) or (a>b) = {(a>b) or (a>b)}") # OR
+
+print(f"not (a>b) = {not (a>b)}") # NOT
