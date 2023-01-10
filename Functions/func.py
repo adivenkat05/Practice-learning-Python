ca4a41d1c7670efc536e70f4c710585b3663a671
Adi Venkat <68367441+adivenkat05@users.noreply.github.com>
2023-01-10 21:48:58 +0530

    Initial commit

---
+++ Functions/func.py
@@ -0,0 +1,11 @@
+import module
+
+print(f"10 + 5 = {module.plus(10, 5)}")
+
+print(f"10 - 5 = {module.minus(10, 5)}")
+
+print(f"5 x 10 = {module.into(5, 10)}")
+
+print(f"10 / 5 = {module.divide(10, 5)}")
+
+print(f"10 % 5 = {module.remainder(10, 5)}")
