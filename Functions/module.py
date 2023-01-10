b9abba08601712af79552b9a921ecb527b9a77b3
Adi Venkat <68367441+adivenkat05@users.noreply.github.com>
2023-01-10 21:48:27 +0530

    Initial commit

---
+++ Functions/module.py
@@ -0,0 +1,17 @@
+# Void func - Returns nothing.
+# Non-void func - Returns value.
+
+def plus(x, y):
+    return x + y
+
+def minus(x, y):
+    return x - y    
+
+def into(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def remainder(x, y):
+    return x % y
